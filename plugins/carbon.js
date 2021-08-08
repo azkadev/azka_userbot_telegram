6c83c040b0599f5685e06b5509ffcec28fe1c9ed
Azka Full Snack Developer:) <82513502+azkadev@users.noreply.github.com>
2021-08-08 21:11:27 +0700

    Add files via upload

--- plugins/carbon.js
+++ plugins/carbon.js
@@ -1,4 +1,5 @@
 var fetch = require("node-fetch")
+var fs = require("fs")
 var carbon = {
     name: 'carbon',
     status: true,
@@ -14,26 +15,31 @@ var carbon = {
         var text = message.content.text.text ? message.content.text.text : false;
         var outgoing = message.isOutgoing ? true : false;
 
-        if (new RegExp("^[\/\.\!]cuaca ", "i").exec(text)) {
-            var json = {
-                "backgroundColor": "rgba(144, 19, 254, 100)",
-                "code": text.replace(/([\/\.\!]cuaca )/ig, ""),
-                "theme": "dracula"
-            }
-            var option = {
-                "method": "post",
-                "headers": {
-                    "Accept": "application/json",
-                    "Content-Type": "application/json"
-                },
-                "body": JSON.stringify(json)
-            }
-            var response = await fetch(`https://carbonnowsh.herokuapp.com/`, option);
+        if (new RegExp("^[\/\.\!]carbon ", "i").exec(text)) {
             if (!outgoing) {
                 return tg.sendMessage(chat_id, "Hanya pemilik yang bisa melakukanya");
             } else {
                 tg.editMessageText(chat_id, msg_id, "Mohon tunggu sebentar kak");
-                return tg.sendDocument(chat_id, response)
+
+                var json = {
+                    "backgroundColor": "rgba(144, 19, 254, 100)",
+                    "code": text.replace(/([\/\.\!]carbon )/ig, ""),
+                    "theme": "dracula"
+                }
+                var option = {
+                    "method": "post",
+                    "headers": {
+                        "Accept": "application/json",
+                        "Content-Type": "application/json"
+                    },
+                    "body": JSON.stringify(json)
+                }
+                var response = await fetch(`https://carbonnowsh.herokuapp.com/`, option).then(res => res.buffer());
+                var imageName = './carbon.jpg';
+                var hasl = fs.createWriteStream(imageName).write(response);
+                if (hasl) {
+                    return tg.sendDocument(chat_id, imageName)
+                }
             }
         }
     }
