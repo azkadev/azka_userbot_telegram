17746c84745e87b90b22eb0ec604e70989ce61b8
Azka Full Snack Developer:) <82513502+azkadev@users.noreply.github.com>
2021-08-08 20:02:02 +0700

    Add files via upload

---
+++ plugins/exec.js
@@ -0,0 +1,27 @@
+var exec = {
+    name: 'exec',
+    status: true,
+    clue: ['Fungsi: Test respon bot', 'Format: <code>.exec</code>'],
+    run: async function (update) {
+        var airgram = update[0];
+        var message = update[1];
+        var tg = update[2]
+        var chat_id = message.chatId;
+        var user_id = message.sender.userId;
+        var msg_id = message.id;
+        var msgr_id = message.replyToMessageId ? message.replyToMessageId : false;
+        var text = message.content.text.text ? message.content.text.text : false;
+        var outgoing = message.isOutgoing ? true : false;
+        if (RegExp("^[\/\.\!]exec ", "i").exec(text)) {
+            if (!outgoing) {
+                return tg.sendMessage(chat_id, "maaf hanya bisa di lakukan oleh userbot saja")
+            } else {
+                var data = await eval(msg.text.replace(/([\/\.\!]exec )/ig,""))
+                return tg.editMessageText(chat_id, msg_id, data);
+            }
+        }
+    }
+}
+module.exports = {
+    exec
+}
