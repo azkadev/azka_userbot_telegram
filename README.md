003999a8225e97990c1f5e4b2108517f98d63c34
azkadev <azkadev@pop-os.localdomain>
2021-10-05 19:27:24 +0700

    patch

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 [Cara Deploy](https://github.com/azkadev/azka-userbot-telegram)
+
+
+## APP id / app hash
+if you don't have a lot time you can use this app
+```bash
+app_id=273729
+app_hash=0f7a4f1ed6c06469bf0ecf70ce92b49d
+```
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var { app_id, app_hash, tdbinlog } = process.env;
 var pluginscript = require("plugins-script");
 var plugin = new pluginscript.plugins("./plugins/");
 var { telegram, update } = require("airgram-lib");
-const shelljs = require("shelljs")
+const shelljs = require("shelljs");
 var fs = require("fs");
 class convert {
   constructor() {
@@ -26,11 +26,11 @@ class convert {
 }
 
 var lib = new convert();
-var sudah = false
+var sudah = false;
 if (tdbinlog) {
   if (!sudah) {
-    lib.base64_decode(tdbinlog, "./db/td.binlog")
-    sudah = true
+    lib.base64_decode(tdbinlog, "./db/td.binlog");
+    sudah = true;
   }
 }
 
@@ -40,10 +40,10 @@ const airgram = new Airgram({
   apiHash: String(app_hash),
   command: "./libtdjson.so",
   logVerbosityLevel: 0
-})
+});
 
 var tg = new telegram(airgram.api);
-var tgUpdate = new update(tg)
+var tgUpdate = new update(tg);
 airgram.use(new Auth({
   code: () => prompt('Please enter the secret code:\n'),
   phoneNumber: () => prompt('Please enter your phone number:\n')
@@ -58,21 +58,21 @@ airgram.on('updateNewMessage', async function ({ update }) {
       var chat_type = msg.chat.type;
       var text = msg.text;
       var msg_id = msg.message_id;
-      var msgr = msg.reply_to_message ? msg.reply_to_message : false;
+      var msgr = (msg.reply_to_message)?msg.reply_to_message : false;
       var outgoing = msg.outgoing ? true : false;
 
       if (RegExp("^[\/\.\!]start$", "i").exec(text)) {
         if (!outgoing) {
-          return tg.sendMessage(chat_id, `Perkenalkan saya adalah bot @azkadev`)
+          return tg.sendMessage(chat_id, `Perkenalkan saya adalah bot @azkadev`);
         } else {
           return tg.editMessageText(chat_id, msg_id, `Perkenalkan saya adalah bot @azkadev`);
         }
       }
       if (RegExp("^[\/\.\!]ping$", "i").exec(text)) {
         var time = (Date.now() / 1000) - msg.date
-        var data = `Pong ${time.toFixed(3)}`
+        var data = `Pong ${time.toFixed(3)}`;
         if (!outgoing) {
-          return tg.sendMessage(chat_id, data)
+          return tg.sendMessage(chat_id, data);
         } else {
           return tg.editMessageText(chat_id, msg_id, data);
         }
@@ -88,8 +88,8 @@ airgram.on('updateNewMessage', async function ({ update }) {
         } else {
           var send = await tg.editMessageText(chat_id, msg_id, "berhasil mengbackup data");
           if (send) {
-            console.log(lib.base64_encode("./td.binlog"))
-            return await tg.sendDocument(chat_id, "./td.binlog", "ini td bin log nya\nData encode nya ada di console logs ya")
+            console.log(lib.base64_encode("./td.binlog"));
+            return await tg.sendDocument(chat_id, "./td.binlog", "ini td bin log nya\nData encode nya ada di console logs ya");
           }
         }
       }
@@ -98,39 +98,38 @@ airgram.on('updateNewMessage', async function ({ update }) {
         if (new RegExp("^\/help$", "i").exec(text)) {
           var teks = await plugin.all();
           if (!outgoing) {
-            return tg.sendMessage(chat_id, teks)
+            return tg.sendMessage(chat_id, teks);
           } else {
             return tg.editMessageText(chat_id, msg_id, teks);
           }
         } else if (/([\/\.\!]help \w+)/i.exec(text)) {
-          var teks = await plugin.help(text.replace(/([\/\.\!]help )/ig, ""))
+          var teks = await plugin.help(text.replace(/([\/\.\!]help )/ig, ""));
           if (!outgoing) {
-            return tg.sendMessage(chat_id, teks)
+            return tg.sendMessage(chat_id, teks);
           } else {
             return tg.editMessageText(chat_id, msg_id, teks);
           }
         } else {
-          var plugins = []
+          var plugins = [];
           fs.readdirSync(require("path").join(__dirname, "./plugins/")).forEach(function (file) {
             var data = require("./plugins/" + file);
-            plugins.push(data)
-          })
-          var jumlah = 0
-          var data_plugin = []
+            plugins.push(data);
+          });
+          var jumlah = 0;
+          var data_plugin = [];
           plugins.forEach(function (plugin) {
             for (var key in plugin) {
               if (Object.prototype.hasOwnProperty.call(plugin, key)) {
                 var data_json = plugin[key];
-                data_plugin.push(data_json)
+                data_plugin.push(data_json);
               }
             }
-          })
+          });
           data_plugin.forEach(function (plugin) {
             if (plugin.status) {
-              return plugin.run(airgram, msg, tg)
-
+              return plugin.run(airgram, msg, tg);
             }
-          })
+          });
         }
       }
     }
