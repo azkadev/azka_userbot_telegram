484d41dc43dde88a0208b88de7d0fc86b495f398
Your Name <you@example.com>
2024-01-08 18:23:12 +0700

    update

---
+++ bin/azka_userbot_telegram.dart
@@ -0,0 +1,5 @@
+import 'package:azka_userbot_telegram/azka_userbot_telegram.dart' as azka_userbot_telegram;
+
+void main(List<String> arguments) {
+  print('Hello world: ${azka_userbot_telegram.calculate()}!');
+}
---
+++ test/azka_userbot_telegram_test.dart
@@ -0,0 +1,8 @@
+import 'package:azka_userbot_telegram/azka_userbot_telegram.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
